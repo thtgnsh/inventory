193cac848ce2052b8c26e5f2db46a85e46baf487
rohitganesh <thtgnsh5@gmail.com>
2021-08-17 14:37:09 +0400

    feature201: v1.1 adding menu

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """This module is the main module that gets lanchued when the user wants
  to run the inventory application
 
- this module will have method to display commandline optionand menus
+this module will have method to display commandline optionand menus
 """
+
+def menu():
+    pass
 
 if __name__ == "__main__":
     pass
