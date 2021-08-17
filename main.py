13696067a209c3176d0e5f94f3c9614c09304c0e
rohitganesh <thtgnsh5@gmail.com>
2021-08-17 14:26:53 +0400

    feature: v1.0 addes application version

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """This module is the main module that gets lanchued when the user wants
- to run the inventory application
+to run the inventory application
 
- this module will have method to display commandline optionand menus
+This module will have method to display commandline options and menus
 """
 def application_version():
     return "v1.0"
-
     
+
 if __name__ == "__main__":
     pass
 
