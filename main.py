947ead3f3c89b4cbf8fe4f51887386ca5ca90da0
rohitganesh <thtgnsh5@gmail.com>
2021-08-17 13:45:20 +0400

    added main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+"""This module is the main module that gets lanchued when the user wants
+ to run the inventory application
+
+ this module will have method to display commandline optionand menus
+"""
+
+if __name__ == "__main__":
+    pass
